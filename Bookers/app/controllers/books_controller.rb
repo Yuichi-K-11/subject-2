6cc5517e5b7462413f06c14c28ba77e8de0f61b9
Yuichi-K-11 <banaichi1@gmail.com>
2020-11-12 14:55:43 +0000

    [add]app1

--- Bookers/app/controllers/books_controller.rb
+++ Bookers/app/controllers/books_controller.rb
@@ -1,13 +1,25 @@
 class BooksController < ApplicationController
   def index
   end
+  
+  def create
+    blog = Book.new(book_params)
+    book.save
+    redirect_to books_path  
+  end
 
   def show
   end
 
   def new
+    @book = Book.new
   end
 
   def edit
+  end
+  
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
