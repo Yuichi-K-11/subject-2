737f22a8b93945c18a54fb484b7cb2a1f3ec45bc
Yuichi-K-11 <banaichi1@gmail.com>
2020-11-14 14:06:54 +0000

    [update]app1

--- Bookers/app/controllers/books_controller.rb
+++ Bookers/app/controllers/books_controller.rb
@@ -1,21 +1,54 @@
 class BooksController < ApplicationController
+  def top
+    
+  end
+  
   def index
+    @books = Book.all
+    @book = Book.new
   end
   
   def create
-    blog = Book.new(book_params)
-    book.save
-    redirect_to books_path  
+    @book = Book.new(book_params)
+    
+    
+    if @book.save
+      flash[:notice] =  "Book was successfully created."
+      redirect_to book_path(@book.id)
+    end
+    
   end
-
-  def show
-  end
-
-  def new
-    @book = Book.new
-  end
-
+  
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  
+  def show
+    @book = Book.find(params[:id])
+    
+     
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    
+    if book.update(book_params)
+      flash[:notice] =  "Book was successfully updated."
+      redirect_to book_path
+    end
+    
+  end  
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    
+    if book.destroy
+      flash[:notice] =  "Book was successfully destroyed."
+      redirect_to books_path
+    end
   end
   
   private
