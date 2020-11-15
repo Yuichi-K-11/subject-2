c8caef4153a9212e2dd69624e24fb23c5aff2b1e
Yuichi-K-11 <banaichi1@gmail.com>
2020-11-15 06:22:25 +0000

    [finish]app1

--- Bookers/app/controllers/books_controller.rb
+++ Bookers/app/controllers/books_controller.rb
@@ -15,6 +15,10 @@ class BooksController < ApplicationController
     if @book.save
       flash[:notice] =  "Book was successfully created."
       redirect_to book_path(@book.id)
+      
+    else
+      @books = Book.all
+      render action: :index
     end
     
   end
@@ -31,12 +35,14 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
+    @book = Book.find(params[:id])
     
-    if book.update(book_params)
+    if @book.update(book_params)
       flash[:notice] =  "Book was successfully updated."
       redirect_to book_path
+    
+    else
+      render action: :edit
     end
     
   end  
--- Bookers/config/routes.rb
+++ Bookers/config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   resources :books
-  
   get "/" => "books#top"
-  
+  root to: 'book#top'
 end
